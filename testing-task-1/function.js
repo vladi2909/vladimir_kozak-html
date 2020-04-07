d7102278877454e7477345038d11e4fb22690a0f
vladimir <52894331+vladi2909@users.noreply.github.com>
2020-04-07 21:07:32 +0300

    feat: add tests

---
+++ testing-task-1/function.js
@@ -0,0 +1,9 @@
+function sum(a, b) {
+
+    if (isNaN(a) || isNaN(b)) {
+      return false;
+    }
+
+    return Number(a) + Number(b);
+
+  }
---
+++ testing-task-1/test.js
@@ -0,0 +1,55 @@
+describe("sum", function() {
+
+  describe("для целых чисел:", function() {
+
+    it("test1", function() {
+      assert.equal(sum(10, 10), 20);
+    });
+
+    it("test2", function() {
+      assert.equal(sum(1e9, 1e9), 2e9);
+    });
+
+  });
+
+  describe("Для дробных чисел:", function() {
+
+    it('test1', function() {
+      assert.equal(sum(1.25, 3.35), 4.6);
+    });
+
+  });
+
+  describe("На число в строке:", function() {
+
+    it('test1', function() {
+      assert.equal(sum('3', 4), 7);
+    });
+
+  });
+
+  describe("На строку:", function() {
+
+    it('test1', function() {
+      assert.equal(sum('odin', 1), false);
+    });
+
+  });
+
+  describe("null , undefined, true/false", function() {
+
+    it('test1', function() {
+      assert.equal(sum(null, 1), 1);
+    });
+
+    it('test2', function() {
+      assert.equal(sum(true, false), 1);
+    });
+
+    it('test3', function() {
+      assert.equal(sum(undefined, 1), false);
+    });
+
+  });
+
+});

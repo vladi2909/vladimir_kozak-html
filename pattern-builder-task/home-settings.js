ec8db599e7e905fe2b070fe72fed7c53a7c05489
vladimir <52894331+vladi2909@users.noreply.github.com>
2020-04-16 00:26:48 +0300

    fix: added graphics

--- pattern-builder-task/home-settings.js
+++ pattern-builder-task/home-settings.js
@@ -1,31 +1,57 @@
 class HomeSettingsBuilder {
     addBasement(basement) {
         this._basement = basement;
+
+        let bas = `
+        ████████████████████████████████
+        ██░░░░░░░░░░░░░░░░░░░░░░░░░░░░██
+        ██░░░░██████████░░░░░░░░░░░░░░██
+        ██░░░░██░░░░░░██░░░░░░░░░░░░░░██
+        ██░░░░████░░░░██░░░░░░░░░░░░░░██
+        ██░░░░██░░░░░░██░░░░░░░░░░░░░░██
+        ██░░░░██░░░░░░██░░░░░░░░░░░░░░██
+        ████████████████████████████████`;
+
+        console.log(bas);
+
         return this;
     }
 
     addFloor(floor) {
         this._floor = floor;
-        return this;
-    }
+        let str = '';
+        let floor_img =  `
+        ████████████████████████████████
+        ██░░░░░░░░░░░░░░░░░░░░░░░░░░░░██
+        ██░░░░██████░░░░░░░░██████░░░░██
+        ██░░░░██████░░░░░░░░██████░░░░██
+        ██░░░░░░░░░░░░░░░░░░░░░░░░░░░░██
+        ██░░░░░░░░░░░░░░░░░░░░░░░░░░░░██
+        ██░░░░░░░░░░░░░░░░░░░░░░░░░░░░██
+        ████████████████████████████████`; 
 
-    addSection(section) {
-        this._section = section;
+        for(let i = 0; i < floor; i++) {
+                str += floor_img;
+            }
+
+        console.log(str);
+
         return this;
     }
 
     addRoof(roof) {
         this._roof = roof;
-        return this;
-    }
 
-    addWindow(window) {
-        this._window = window;
-        return this;
-    }
+        let roof_img = `
+                      ████
+                ████████████████
+              ████████████████████
+            ████████████████████████
+          ████████████████████████████
+        ████████████████████████████████`;
+    
+        console.log(roof_img);
 
-    addDoor(door) {
-        this._door = door;
         return this;
     }
 
@@ -38,29 +64,14 @@ class HomeSettingsBuilder {
             throw new Error("Please set floor");
         }
 
-        if (!this._section) {
-            throw new Error("Please set section");
-        }
-
         if (!this._roof) {
             throw new Error("Please set roof");
-        }
-
-        if (!this._window) {
-            throw new Error("Please set window");
-        }
-
-        if (!this._door) {
-            throw new Error("Please set door");
         }
 
         return new HomeSettings({
             basement: this._basement,
             floor: this._floor,
-            section: this._section,
             roof: this._roof,
-            window: this._window,
-            door: this._door
         });
     }
 }
@@ -69,10 +80,7 @@ class HomeSettings {
     constructor(settings) {
         this._basement = settings.basement;
         this._floor = settings.floor;
-        this._section = settings.section;
         this._roof = settings.roof;
-        this._window = settings.window;
-        this._door = settings.door;
     }
 
     get basement() {
@@ -83,20 +91,8 @@ class HomeSettings {
         return this._floor;
     }
 
-    get section() {
-        return this._section;
-    }
-
     get roof() {
         return this._roof;
-    }
-
-    get window() {
-        return this._window;
-    }
-
-    get door() {
-        return this._door;
     }
 
     static getBuilder() {
--- pattern-builder-task/index.js
+++ pattern-builder-task/index.js
@@ -1,12 +1,7 @@
 const HomeSettings = require('./home-settings');
 
 const home = HomeSettings.getBuilder()
-    .addBasement(true) 
-    .addFloor(3)
-    .addSection(2)
     .addRoof('gable')
-    .addWindow(12)
-    .addDoor(9)
+    .addFloor(2)
+    .addBasement(true) 
     .build();
-
-console.log(home);

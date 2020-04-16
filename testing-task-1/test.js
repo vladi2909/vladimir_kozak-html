6814929c24565317b53a187dcae12cfa1ab1aa5f
vladimir <52894331+vladi2909@users.noreply.github.com>
2020-04-16 12:16:41 +0300

    feat: add tests (#8)
    * feat: add tests
    
    * fix: modified items
    
    * fix: add tests
    
    * fix: add tests
    
    * Delete debug.log
    
    * fix: return value
    
    Co-authored-by: FE-2020-TRAINERS <61979915+FE-2020-TRAINERS@users.noreply.github.com>

---
+++ testing-task-1/test.js
@@ -0,0 +1,55 @@
+describe("sum", function() {
+
+  describe("for integer:", function() {
+
+    it("test1", function() {
+      assert.equal(sum(10, 10), 20);
+    });
+
+    it("test2", function() {
+      assert.equal(sum(1e9, 1e9), 2e9);
+    });
+
+  });
+
+  describe("for floats:", function() {
+
+    it('test1', function() {
+      assert.equal(sum(1.25, 3.35), 4.6);
+    });
+
+  });
+
+  describe("number in a line:", function() {
+
+    it('test1', function() {
+      assert.equal(sum('3', 4), 7);
+    });
+
+  });
+
+  describe("for line:", function() {
+
+    it('test1', function() {
+      assert.equal(sum('odin', 1), 'NaN');
+    });
+
+  });
+
+  describe("null , undefined, true/false", function() {
+
+    it('test1', function() {
+      assert.equal(sum(null, 1), 1);
+    });
+
+    it('test2', function() {
+      assert.equal(sum(true, false), 1);
+    });
+
+    it('test3', function() {
+      assert.equal(sum(undefined, 1), 'NaN');
+    });
+
+  });
+
+});

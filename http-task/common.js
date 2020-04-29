59bb4e5d6b6d0bec15d7f3d714e0336fc16ba4b4
vladimir <vladi2909@outlook.com>
2020-04-29 23:48:40 +0300

    fix: add patterns

--- http-task/common.js
+++ http-task/common.js
@@ -1,85 +1,121 @@
-const getBtn = document.getElementById('getBtn');
-const postBtn = document.getElementById('postBtn');
-const putBtn = document.getElementById('putBtn');
-const delBtn = document.getElementById('delBtn');
-const result = document.getElementById('result');
-
 const requestURL = 'https://jsonplaceholder.typicode.com/users';
 const item = '/1';
 
+const module = (function () {
+    
+    return {
 
-// CREATE
+        privateClass: class Singleton {
+            constructor() {
+                const instance = this.constructor.instance;
+                if (instance) {
+                    return instance;
+                }
 
-const sendHttpRequest = (method, url, data) => {
-    return fetch(url, {
-        method: method,
-        body: JSON.stringify(data),
-        headers: data ? {'Content-Type': 'application/json'} : {}
-    }).then(res => {
-        if (res.status >= 400) {
-            // !response.ok
-            return res.json().then(errResData => {
-                const error = new Error('Something went wrong...');
-                error.data = errResData;
-                throw error;
-            });
-        }
-        return res.json();
-    });
-};
+                this.constructor.instance = this;
+            }
 
 
-// GET 
+            // CREATE
 
-const getData = () => {
-    sendHttpRequest('GET', requestURL)
-        .then(res => {
-            res.map(user => {
-                result.innerHTML += `<p>${user.id}: Name: ${user.name}, Surname: ${user.username}, Address: ${user.address.street}</p>`;
-            });
-        });
-};
-
-// POST
-
-const sendData = () => {
-    sendHttpRequest('POST', requestURL, {
-        name: 'Vladimir',
-        username: 'Kozak'
-    })
-    .then(resData => {
-        result.innerHTML += `<p>POST DATA: ${resData.id}: ${resData.name}, ${resData.username}</p>`;
-    })
-    .catch(err => {
-        console.log(err, err.data);
-    });
-};
+            sendHttpRequest(method, url, data) {
+                return fetch(url, {
+                    method: method,
+                    body: JSON.stringify(data),
+                    headers: data ? {'Content-Type': 'application/json'} : {}
+                }).then(res => {
+                    if (res.status >= 400) {
+                        // !response.ok
+                        return res.json().then(errResData => {
+                            const  error = new Error('Something went wrong...');
+                            error.data = errResData;
+                            throw error;
+                        });
+                    }
+                    return res.json();
+                });
+            }
 
 
-// PUT
+            // GET
 
-const putData = () => {
-    sendHttpRequest('PUT', requestURL + item, {
-        name: 'Vladimir',
-        username: 'Kozak',
-        job: 'student'
-    }).then(res => {
-        result.innerHTML += `<p>UPDATE: ${res.name} ${res.job}</p>`;
-    })
-};
+            getData() {
+                return this.sendHttpRequest('GET', requestURL)
+                    .then(res => {
+                        res.map(user => {
+                            resultDiv.innerHTML += `<p>${user.id}: Name: ${user.name}, Surname: ${user.username}, Address: ${user.address.street}</p>`;
+                        });
+                    });
+            } 
+        
+
+            // POST
+
+            sendData() {
+                return this.sendHttpRequest('POST', requestURL, {
+                    name: 'Vladimir',
+                    username: 'Kozak'
+                })
+                .then(resData => {
+                    resultDiv.innerHTML += `<p> POST DATA: ${resData.id}: ${resData.name}, ${resData.username} </p>`;
+                })
+                .catch(err => {
+                    console.log(err, err.data);
+                });
+            }
 
 
-// DELETE
+            // PUT
 
-const delData = () => {
-    sendHttpRequest('DELETE', requestURL + item)
-    .then(() => {
-        result.innerHTML += 'DATA DELETED';
-    })
-};
+            putData() {
+                return this.sendHttpRequest('PUT', requestURL + item, {
+                    name: 'Vladimir',
+                    username: 'Kozak',
+                    job: 'student'
+                }).then(res => {
+                    resultDiv.innerHTML += `<p> PUT: Name: ${res.name}, Job: ${res.job} </p>`;
+                });
+            }
 
 
-getBtn.addEventListener('click', getData);
-postBtn.addEventListener('click', sendData);
-putBtn.addEventListener('click', putData);
-delBtn.addEventListener('click', delData);
+            // DELETE
+
+            delData() {
+                return this.sendHttpRequest('DELETE', requestURL + item)
+                    .then(() => {
+                        resultDiv.innerHTML += 'DATA DELETED';
+                    })
+                }
+            }
+    }
+})();
+
+let user = new module.privateClass();
+let user2 = new module.privateClass();
+console.log(user === user2); //true
+
+
+// BUTTONS
+
+const getBtn = document.getElementById('getBtn');
+getBtn.onclick = function() {
+    user.getData();
+}
+
+const sendBtn = document.getElementById('sendBtn');
+sendBtn.onclick = function() {
+    user.sendData();
+}
+
+const putBtn = document.getElementById('putBtn');
+putBtn.onclick = function() {
+    user.putData();
+}
+
+const delBtn = document.getElementById('delBtn');
+delBtn.onclick = function() {
+    user.delData();
+}
+
+const resultDiv = document.getElementById('resultDiv');
+

4ec941bde37f8dcc847d9572f4ff86ce4f4c3764
vladimir <vladi2909@outlook.com>
2020-05-12 01:25:12 +0300

    fix: add component

--- angular-task-1/my-app/src/app/app.module.ts
+++ angular-task-1/my-app/src/app/app.module.ts
@@ -1,17 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TodolistComponent } from './todolist/todolist.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodolistComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
+  exports: [],
   providers: [],
   bootstrap: [AppComponent]
 })

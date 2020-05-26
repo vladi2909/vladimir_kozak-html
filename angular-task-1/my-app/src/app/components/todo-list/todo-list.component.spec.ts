51fc95cd7012feb6a092dbf38587b11e29e3a45b
vladimir <52894331+vladi2909@users.noreply.github.com>
2020-05-26 14:42:10 +0300

    feat: moved the project, add functional (#17)
    Co-authored-by: vladimir <vladi2909@outlook.com>

--- angular-task-1/my-app/src/app/components/todo-list/todo-list.component.spec.ts
+++ angular-task-1/my-app/src/app/components/todo-list/todo-list.component.spec.ts
@@ -1,20 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { TodolistComponent } from './todolist.component';
+import { TodoListComponent } from './todo-list.component';
 
-describe('TodolistComponent', () => {
-  let component: TodolistComponent;
-  let fixture: ComponentFixture<TodolistComponent>;
+describe('TodoListComponent', () => {
+  let component: TodoListComponent;
+  let fixture: ComponentFixture<TodoListComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ TodolistComponent ]
+      declarations: [ TodoListComponent ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(TodolistComponent);
+    fixture = TestBed.createComponent(TodoListComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
